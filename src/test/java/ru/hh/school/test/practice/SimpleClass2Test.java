cd348e2b7f996f91a5d6329bdae646943d6b77b6
alinkov <aleksanderlinkov@gmail.com>
2019-02-06 00:02:50 +0300

    Pitest 100% coverage

--- src/test/java/ru/hh/school/test/practice/SimpleClass2Test.java
+++ src/test/java/ru/hh/school/test/practice/SimpleClass2Test.java
@@ -1,10 +1,14 @@
 package ru.hh.school.test.practice;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SimpleClass2Test {
 
@@ -22,6 +26,16 @@ public class SimpleClass2Test {
     @Test
     public void testDoMultiplyNull() {
         assertEquals(0.0, SimpleClass.doMultiply(null), 0.000001);
+    }
+
+    @Test
+    @Ignore
+    public void coverage100DontDoIt() throws Exception {
+        System.out.println("Don't do it! It's only for 100% Pitest coverage");
+        Constructor<SimpleClass> constructor = SimpleClass.class.getDeclaredConstructor();
+        assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+        constructor.setAccessible(true);
+        constructor.newInstance();
     }
 
 }
